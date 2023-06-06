e76e6aa1f17c46ef812d3fb68527be05d7d3072d
João Victor Dos Santos Ferreira <joaovictor2111@gmail.com>
2023-06-06 00:23:12 -0300

    feat: login and sign up

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -2,6 +2,7 @@ import { useContext, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "../../context/authContext";
 import "./login.scss";
+import loginImage from '../../assets/login-animated.png';
 
 const Login = () => {
   const [inputs, setInputs] = useState({
@@ -28,41 +29,39 @@ const Login = () => {
   };
 
   return (
-    <div className="login">
-      <div className="card">
-        <div className="left">
-          <h1>Hello World.</h1>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero cum,
-            alias totam numquam ipsa exercitationem dignissimos, error nam,
-            consequatur.
-          </p>
-          <span>Ainda não tem uma conta DevFinder?</span>
-          <Link to="/register">
-            <button>Registre-se</button>
-          </Link>
-        </div>
-        <div className="right">
-          <h1>Login</h1>
-          <form>
-            <input
-              type="text"
-              placeholder="Username"
-              name="username"
-              onChange={handleChange}
-            />
-            <input
-              type="password"
-              placeholder="Password"
-              name="password"
-              onChange={handleChange}
-            />
-            {err && err}
-            <button onClick={handleLogin}>Login</button>
-          </form>
+    <div className="container">
+      <div className="content">
+          <div className="left">
+            <h1>Como é bom ter você <br /> de volta!</h1>
+            <img src = {loginImage} /> 
+          </div>
+          <div className="form">
+            <h1>Login</h1>
+            <form>
+              <input
+                type="text"
+                placeholder="Username"
+                name="username"
+                onChange={handleChange}
+              />
+              <input
+                type="password"
+                placeholder="Password"
+                name="password"
+                onChange={handleChange}
+              />
+              {err && err}
+              <button onClick={handleLogin}>Login</button>
+            </form>
+            <div className="redirect">
+              <span>Ainda não tem uma conta DevFinder?</span>
+              <Link to="/register">
+                <p>Registre-se</p>
+              </Link>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
   );
 };
 
--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -1,7 +1,9 @@
+import React from "react";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import "./register.scss";
 import axios from "axios";
+import animatedSignUp from '../../assets/animated-signup.png';
 
 const Register = () => {
   const [inputs, setInputs] = useState({
@@ -29,51 +31,53 @@ const Register = () => {
   console.log(err)
 
   return (
-    <div className="register">
-      <div className="card">
-        <div className="left">
-          <h1>DevFinder.</h1>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero cum,
-            alias totam numquam ipsa exercitationem dignissimos, error nam,
-            consequatur.
-          </p>
-          <span>Do you have an account?</span>
-          <Link to="/login">
-            <button>Login</button>
-          </Link>
-        </div>
-        <div className="right">
-          <h1>Register</h1>
-          <form>
-            <input
-              type="text"
-              placeholder="Username"
-              name="username"
-              onChange={handleChange}
-            />
-            <input
-              type="email"
-              placeholder="Email"
-              name="email"
-              onChange={handleChange}
-            />
-            <input
-              type="password"
-              placeholder="Password"
-              name="password"
-              onChange={handleChange}
-            />
-            <input
-              type="text"
-              placeholder="Name"
-              name="name"
-              onChange={handleChange}
-            />
-            {err && err}
-            <button onClick={handleClick}>Registre-se</button>
-          </form>
-        </div>
+    <div className="container">
+      <div className="content">
+          <div className="left">
+            <h1>Entre para a nossa <br /> comunidade</h1>
+            <img src={animatedSignUp} alt = "illustration"  />
+          </div>
+
+          <div className="form">
+            <h1>Registre-se</h1>
+            <form>
+              <input
+                type="text"
+                placeholder="Username"
+                name="username"
+                onChange={handleChange}
+              />
+              <input
+                type="email"
+                placeholder="Email"
+                name="email"
+                onChange={handleChange}
+              />
+              <input
+                type="password"
+                placeholder="Password"
+                name="password"
+                onChange={handleChange}
+              />
+              <input
+                type="text"
+                placeholder="Name"
+                name="name"
+                onChange={handleChange}
+              />
+              {err && err}
+              <button onClick={handleClick}>Registre-se</button>
+            </form>
+            <div className="redirect">
+              <span>Já possui uma conta?</span>
+              <Link to="/login">
+                <p>Login</p>
+              </Link>
+            </div>
+           
+
+          </div>
+
       </div>
     </div>
   );
